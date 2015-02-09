57eefed3214fb2508f6a9f667668b48fb450c3fb
Diana.Diaconu <diana.diaconu@teamnet.ro>
2015-02-09 17:26:32 +0200

    service+serviceImpl+controller for REST operations

---
+++ src/main/java/ro/teamnet/bootstrap/service/AbstractServiceImpl.java
@@ -0,0 +1,56 @@
+package ro.teamnet.bootstrap.service;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.transaction.annotation.Transactional;
+import ro.teamnet.bootstrap.extend.AppRepository;
+
+import java.io.Serializable;
+import java.util.List;
+
+
+public abstract class AbstractServiceImpl<T extends Serializable> implements AbstractService<T> {
+
+
+         private final Logger log = LoggerFactory.getLogger(AbstractServiceImpl.class);
+
+         private final AppRepository<T,Long> repository;
+
+         public AbstractServiceImpl(AppRepository<T,Long> repository){
+           this.repository=repository;
+         }
+
+
+        @Override
+        @Transactional
+        public void save(T t){
+            log.debug("REST request to save : {}", t);
+            repository.save(t) ;
+        }
+
+
+
+        @Override
+        public List<T> findAll(){
+            log.debug("REST request to get all records");
+            return repository.findAll();
+        }
+
+
+
+        @Override
+        public T findOne(Long id){
+            log.debug("REST request to get : {}", id);
+            return (T)repository.findOne(id);
+        }
+
+
+        @Override
+        @Transactional
+        public void delete(Long id){
+            log.debug("REST request to delete : {}", id);
+            repository.delete(id);
+        }
+
+
+}
